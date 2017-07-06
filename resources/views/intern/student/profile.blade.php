4d20d5daff922e439e097e94b86040b0d0a98ab6
Qiwen Zhu <qiwzhu@indiana.edu>
2017-07-06 13:59:11 -0400

    add student profile template

---
+++ resources/views/intern/student/profile.blade.php
@@ -0,0 +1,14 @@
+shows info of student, such as
+<p>
+    student names
+</p>
+
+<p>
+    status of internship application processing
+</p>
+<p>
+    things to do, such as submitting journal, and site evaluation
+</p>
+<p>
+    other info
+</p>
