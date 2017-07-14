dd3c7c925a9a436d16f382946baf264cba060b10
Qiwen Zhu <qiwzhu@indiana.edu>
2017-07-14 00:25:44 -0400

    finish decoration for application/organization  and supervisor

--- resources/views/intern/student/application/organization.blade.php
+++ resources/views/intern/student/application/organization.blade.php
@@ -1,33 +1,133 @@
+@extends('layouts.app')
+<?php
+$user_id = Auth::user()->id;
+
+//$organization_id = (int)session('data')['org_id'];
+$organization_id = 198155;
 
 
-here using js in the frontend to check if the organization exists in DB
+//$supervisor_id = (int)session('data')['supervisor_id'];
+$supervisor_id = 198155;
 
-if it does, just pass the id to next view, the supervisor page
+$credit_hours = [
+        '0' => '0',
+        '1' => '1',
+        '2' => '2',
+        '3' => '3',
+];
 
-if not, call insert method of InternOrganizationController to insert a new record
-and pass  id, to supervisor page.
+$countries = [
+        'China' => 'China',
+        'United Kingdom' => 'United Kingdom',
+];
 
-We can use insertGetId method in the model to get the id of the newly-inserted record
+$states = [
+        'state 1' => 'state 1',
+        'state 2' => 'state 2',
+];
+$cities = [
+        'city 1' => 'city 1',
+        'city 2' => 'city 2',
+];
+$work_schedule_guide = 'please briefly describe your anticipated work schedule';
+$duties_guide = 'Provide a detailed description of the internship experience and your specific duties';
+$reasons_guide = 'Explain why this internship/volunteer opportunity was chosen (i.e., how will it help you in your educational and career goals)';
+$culture_guide = 'If it is an internship abroad, explain how you will interact with the host culture';
+$challenge_guide = 'Detail any challenges you expect to face during  the internship and explain how you intend to face these challenges';
+?>
+@section('content')
 
-{!! Form::open(['action' => 'InternOrganizationController@store']) !!}
-
-
-
-
-
-{!! Form::label('organization_name', 'Organization') !!}
-{!! Form::text('organization_name') !!}
-
-{{--here need to check if the url is legal--}}
-{!! Form::label('organization_url', 'Organization Website') !!}
-{!! Form::text('organization_url') !!}
-
-{!! Form::submit('next') !!}
-{!! Form::close() !!}
-
-
-
-
-
+    <link rel="stylesheet" href="/css/test/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/test/demo.css">
+    <link rel="stylesheet" href="/css/test/gsdk-bootstrap-wizard.css">
+    {{--<link rel="stylesheet" href="/css/test/gsdk-base.css">--}}
 
 
+
+    <div class="image-container set-full-height" style="background-image: url('/img/wizard-city.jpg')">
+        <!--   Big container   -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+
+                    <!--      Wizard container        -->
+                    <div class="wizard-container">
+                        <div class="card wizard-card" data-color="azzure" id="wizard">
+                        {{--<form action="" method="">--}}
+                        {!! Form::open(['action' => 'InternOrganizationController@store']) !!}
+
+                        <!--        You can switch ' data-color="azzure" '  with one of the next bright colors: "blue", "green", "orange", "red"          -->
+
+                            <div class="wizard-header">
+                                <h3>
+                                    <b>SGIS Internship Application</b> <br>
+                                    <small>Tell us about your internship.</small>
+                                </h3>
+                            </div>
+                            <div class="wizard-navigation">
+                                <ul>
+                                    <li><a href="#organization" data-toggle="tab">Organization</a></li>
+                                </ul>
+                            </div>
+                            <div class="tab-content">
+
+                                <div class="tab-pane" id="organization">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <h4 class="info-text"> Which <b>organization</b> is the internship with?</h4>
+                                        </div>
+                                        <div class="col-sm-8 col-sm-offset-2">
+                                            <div class="form-group">
+                                                {!! Form::label('organization_name', 'Organization name') !!}
+                                                {!! Form::text('organization_name', null, ['class' => 'form-control']) !!}
+
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-8 col-sm-offset-2">
+                                            <div class="form-group">
+                                                {{--here need to check if the url is legal--}}
+                                                {!! Form::label('organization_url', 'Organization Website URL') !!}
+                                                {!! Form::text('organization_url', 'https://', ['class' => 'form-control']) !!}
+                                                <br>
+                                            </div>
+                                        </div>
+                                        {{--TODO--}}
+                                        {{--We will need organization type --}}
+
+                                    </div>
+                                </div>
+
+                                <!-- end of cards definitions -->
+
+                            </div>
+
+                            <div class="wizard-footer">
+                                <div class="pull-right">
+                                    <input type='button' class='btn btn-next btn-fill btn-info btn-wd btn-sm' name='next' value='Next' />
+                                    {!! Form::submit('Tell us about your supervisor in this organization', ['class' => 'btn btn-finish btn-fill btn-info btn-wd btn-sm', 'name' => 'finish']) !!}
+                                </div>
+                                <div class="pull-left">
+                                    <input type='button' class='btn btn-previous btn-fill btn-default btn-wd btn-sm' name='previous' value='Previous' />
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                            {{--</form>--}}
+                            {!! Form::close() !!}
+                        </div>
+                    </div> <!-- wizard container -->
+                </div>
+            </div> <!-- row -->
+        </div> <!--  big container -->
+
+
+
+
+    </div>
+
+
+    <script src="/js/test/jquery-1.10.2.js"></script>
+    <script src="/js/test/bootstrap.min.js"></script>
+    <script src="/js/test/jquery.validate.min.js"></script>
+    <script src="/js/test/jquery.bootstrap.wizard.js"></script>
+    <script src="/js/test/wizard.js"></script>
+@endsection
--- resources/views/intern/student/application/supervisor.blade.php
+++ resources/views/intern/student/application/supervisor.blade.php
@@ -1,45 +1,126 @@
-
-here using js in the frontend to check if the supervisor exists in DB
-
-if it does, just pass the id and organization id to the application creation page
-
-if not, call insert method of InternSupervisorController to insert a new record
-and pass  id and organization id, to next page.
-
-We can use insertGetId method in the model to get the id of the newly-inserted record
-
+@extends('layouts.app')
 <?php
 $org_id = session('org_id');
 ?>
+@section('content')
 
-{!! Form::open(['action' => 'InternSupervisorController@store']) !!}
-
-{!! Form::label('supervisor_first_name', 'First name') !!}
-{!! Form::text('supervisor_first_name') !!}
-
-{!! Form::label('supervisor_last_name', 'Last name') !!}
-{!! Form::text('supervisor_last_name') !!}
-
-{!! Form::label('supervisor_prefix', 'prefix') !!}
-{!! Form::text('supervisor_prefix') !!}
-
-{!! Form::label('supervisor_email', 'email') !!}
-{!! Form::text('supervisor_email') !!}
-
-{!! Form::label('supervisor_phone_country_code', 'phone: country code') !!}
-{!! Form::text('supervisor_phone_country_code') !!}
-
-{!! Form::label('supervisor_phone', 'phone #') !!}
-{!! Form::text('supervisor_phone') !!}
-
-{!! Form::hidden('org_id', $org_id) !!}
-
-{!! Form::submit('next') !!}
-{!! Form::close() !!}
+    <link rel="stylesheet" href="/css/test/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/test/demo.css">
+    <link rel="stylesheet" href="/css/test/gsdk-bootstrap-wizard.css">
+    {{--<link rel="stylesheet" href="/css/test/gsdk-base.css">--}}
 
 
 
+    <div class="image-container set-full-height" style="background-image: url('/img/wizard-city.jpg')">
+        <!--   Big container   -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+
+                    <!--      Wizard container        -->
+                    <div class="wizard-container">
+                        <div class="card wizard-card" data-color="azzure" id="wizard">
+                        {!! Form::open(['action' => 'InternSupervisorController@store']) !!}
+                        {!! Form::hidden('org_id', $org_id) !!}
+
+                        <!--        You can switch ' data-color="azzure" '  with one of the next bright colors: "blue", "green", "orange", "red"          -->
+
+                            <div class="wizard-header">
+                                <h3>
+                                    <b>SGIS Internship Application</b> <br>
+                                    <small>Tell us about your internship.</small>
+                                </h3>
+                            </div>
+                            <div class="wizard-navigation">
+                                <ul>
+                                    <li><a href="#supervisor" data-toggle="tab">Supervisor</a></li>
+                                </ul>
+                            </div>
+                            <div class="tab-content">
+
+                                <div class="tab-pane" id="supervisor">
+                                    <div class="row">
+                                        <div class="col-sm-12">
+                                            <h4 class="info-text"> Who will be your <b>supervisor</b> in that organization?</h4>
+                                        </div>
+                                        <div class="col-sm-5 col-sm-offset-1">
+                                            <div class="form-group">
+                                                {!! Form::label('supervisor_first_name', 'First name') !!}
+                                                {!! Form::text('supervisor_first_name', null, ['class' => 'form-control']) !!}
+
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-5">
+                                            <div class="form-group">
+                                                {!! Form::label('supervisor_last_name', 'Last name') !!}
+                                                {!! Form::text('supervisor_last_name', null, ['class' => 'form-control']) !!}
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-5 col-sm-offset-1">
+                                            <div class="form-group">
+                                                {!! Form::label('supervisor_prefix', 'prefix') !!}
+                                                {!! Form::text('supervisor_prefix', null, ['class' => 'form-control']) !!}
+
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-5">
+                                            <div class="form-group">
+                                                {!! Form::label('supervisor_email', 'email') !!}
+                                                {!! Form::text('supervisor_email', null, ['class' => 'form-control']) !!}
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-5 col-sm-offset-1">
+                                            <div class="form-group">
+                                                {!! Form::label('supervisor_phone_country_code', 'phone: country code') !!}
+                                                {!! Form::text('supervisor_phone_country_code', null, ['class' => 'form-control']) !!}
+
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-5">
+                                            <div class="form-group">
+                                                {!! Form::label('supervisor_phone', 'phone #') !!}
+                                                {!! Form::text('supervisor_phone', null, ['class' => 'form-control']) !!}
+                                            </div>
+                                        </div>
+
+
+
+                                    </div>
+                                </div>
+
+                                <!-- end of cards definitions -->
+
+                            </div>
+
+                            <div class="wizard-footer">
+                                <div class="pull-right">
+                                    <input type='button' class='btn btn-next btn-fill btn-info btn-wd btn-sm' name='next' value='Next' />
+                                    {!! Form::submit('Give us some other details', ['class' => 'btn btn-finish btn-fill btn-info btn-wd btn-sm', 'name' => 'finish']) !!}
+                                </div>
+                                <div class="pull-left">
+                                    <input type='button' class='btn btn-previous btn-fill btn-default btn-wd btn-sm' name='previous' value='Previous' />
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                            {{--</form>--}}
+                            {!! Form::close() !!}
+                        </div>
+                    </div> <!-- wizard container -->
+                </div>
+            </div> <!-- row -->
+        </div> <!--  big container -->
 
 
 
 
+    </div>
+
+
+    <script src="/js/test/jquery-1.10.2.js"></script>
+    <script src="/js/test/bootstrap.min.js"></script>
+    <script src="/js/test/jquery.validate.min.js"></script>
+    <script src="/js/test/jquery.bootstrap.wizard.js"></script>
+    <script src="/js/test/wizard.js"></script>
+@endsection
