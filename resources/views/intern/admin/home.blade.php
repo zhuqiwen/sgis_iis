516e0bc678bff6bca1bd14f45ae474ec0df9680c
Qiwen Zhu <qiwzhu@indiana.edu>
2017-07-16 16:13:47 -0400

    replace student home template with HTMLSnippet::generateCardRow()

--- resources/views/intern/admin/home.blade.php
+++ resources/views/intern/admin/home.blade.php
@@ -1,4 +1,33 @@
-this is admin home page
+{{--this is admin home page--}}
 
-<a href="application/approve">To approve internship applications</a>
-<a href="application/approve">To review internship evaluations</a>
+{{--<a href="application/approve">To approve internship applications</a>--}}
+{{--<a href="application/approve">To review internship evaluations</a>--}}
+
+
+@extends('layouts.home')
+@section('card_rows')
+    {!! \app\Helpers\HTMLSnippet::
+        generateCardRow(
+            '10%',
+            [
+                ['href' => 'href1', 'title' => 'Approve Applications', 'text' => 'text1'],
+                ['href' => 'href2', 'title' => 'AAAAA', 'text' => 'text2']
+            ]
+        ) !!}
+
+    {!! \app\Helpers\HTMLSnippet::
+        generateCardRow(
+            '5%',
+            [
+                ['href' => 'href3', 'title' => 'BBBBBB', 'text' => 'text3'],
+                ['href' => 'href4', 'title' => 'CCCCCC', 'text' => 'text4']
+            ]
+        ) !!}
+
+@endsection
+
+
+
+
+
+
--- resources/views/test/test.blade.php
+++ resources/views/test/test.blade.php
@@ -1,6 +1,13 @@
 @extends('layouts.home')
 @section('card_rows')
-    {!! \app\Helpers\HTMLSnippet::generateCardRow('10%', [['href' => 'href1', 'title' => 'title1', 'text' => 'text1'],['href' => 'href2', 'title' => 'title2', 'text' => 'text2']]) !!}
+    {!! \app\Helpers\HTMLSnippet::
+        generateCardRow(
+            '10%',
+            [
+                ['href' => 'href1', 'title' => 'title1', 'text' => 'text1'],
+                ['href' => 'href2', 'title' => 'title2', 'text' => 'text2']
+            ]
+        ) !!}
 
 @endsection
 
