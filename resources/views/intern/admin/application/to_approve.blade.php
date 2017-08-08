a63a9be7cf62804bbca66f2254710a3211af9334
zhuqiwen <zqwjulia55@hotmail.com>
2017-08-08 00:09:37 -0400

    add group by button in to_approve view

--- resources/views/intern/admin/application/to_approve.blade.php
+++ resources/views/intern/admin/application/to_approve.blade.php
@@ -4,12 +4,48 @@
 
 <div class="box">
     <div class="box-header with-border">
-        <h3 class="box-title">Internship Applications To Approve</h3>
-        <div class="box-tools pull-right">
+        <div class="row">
+            <div class="col-md-4">
+                {{--<h3 class="box-title">Internship Applications To Approve</h3>--}}
+                <h3 class="box-title">Internship Applications To Approve</h3>
+            </div>
+            <div class="col-md-2">
+                <ul class="nav box-title">
+
+                    <li class="dropdown">
+                        <a href="#"
+                           class="dropdown-toggle"
+                           data-toggle="dropdown"
+                           role="button"
+                           aria-expanded="false">
+                            <h3 class="box-title">Group By
+                            <span class="caret"></span>
+                            </h3>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="#">
+                                    Country
+                                </a>
+
+
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-md-offset-3">
+                <span class="label label-primary">Label</span>
+
+            </div>
+        </div>
+
+        {{--<div class="box-tools pull-right">--}}
             <!-- Buttons, labels, and many other things can be placed here! -->
             <!-- Here is a label for example -->
-            <span class="label label-primary">Label</span>
-        </div>
+
+        {{--</div>--}}
         <!-- /.box-tools -->
     </div>
     <!-- /.box-header -->
